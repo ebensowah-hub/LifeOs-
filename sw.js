aa873fcf0ff92c5d391ca5a6035d54821bee9795
ebensowah-hub <ebenezerokonai@gmail.com>
2026-08-15 04:54:51 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,51 @@
-const C='lifeos-v1';const A=['./','./index.html','./style.css','./app.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C = 'lifeos-v2';
+
+const A = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(C).then(cache => cache.addAll(A))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== C)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(C).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+
+      })
+      .catch(() =>
+        caches.match(event.request)
+      )
+  );
+});
